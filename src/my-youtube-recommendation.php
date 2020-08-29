98f5dd7a0e97dd3a14305c1d6f622ddd142c7371
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-29 12:29:45 -0300

    first commit

---
+++ src/my-youtube-recommendation.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link              https://github.com/gabriellimoni/curso-plugin-wordpress-cdftv
+ * @since             1.0.0
+ * @package           My_Youtube_Recommendation
+ *
+ * @wordpress-plugin
+ * Plugin Name:       My Youtube Recommendation
+ * Plugin URI:        https://github.com/gabriellimoni/curso-plugin-wordpress-cdftv
+ * Description:       Display the last videos from a Youtube channel using Youtube feed and keep always updated even for cached posts.
+ * Version:           1.0.0
+ * Author:            Gabriel Limoni
+ * Author URI:        https://github.com/gabriellimoni
+ * License:           GPLv3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       my-youtube-recommendation
+ * Domain Path:       /languages/
+ */
